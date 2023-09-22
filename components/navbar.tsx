75b330031ee1617d9b6193ee2a609dd9c00e8066
matcha <massahiro.endo@gmail.com>
2023-09-22 10:39:54 +0200

    add: toggle button component in Navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default async function Navbar() {
   const { userId } = auth();
@@ -20,6 +21,7 @@ export default async function Navbar() {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
